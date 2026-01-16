2625b32f8dcf8b4ca764d8ac72266a57529256d7
Leandro Reis <leandro.reis@closer.pt>
2026-01-16 14:34:11 +0000

    add backend structure

---
+++ ssam-app/ssam/src/main/java/com/valeo/ssam/SsamApplication.java
@@ -0,0 +1,13 @@
+package com.valeo.ssam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsamApplication.class, args);
+	}
+
+}
