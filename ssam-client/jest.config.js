2321a28eb1fe6398f44e57e10c4f3e1d4e8be608
Leandro Reis <leandro.reis@closer.pt>
2026-01-19 03:58:37 +0000

    implemented tests

---
+++ ssam-client/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json'
+    }
+  }
+};
